99575859f1dc47455b98c6758964a126bbf8fb67
Anuranjan <itanuranjan@gmail.com>
2024-02-20 00:28:58 +0530

    add new navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
+// import Header from './components/Header/Header';
+// import Home from './components/Home/Home';
+import Navbar from './components/Header/Navbar';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Home />
-      //h1
-      //h2
-      My name is Akanksha
+      {/* <Header /> */}
+      <Navbar/>
+      {/* <Home /> */}
+    
 
     </div>
   );
