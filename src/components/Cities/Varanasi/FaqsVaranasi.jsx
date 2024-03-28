ac035cd53ba2296485e28143378533335399a077
Anuranjan <itanuranjan@gmail.com>
2024-03-28 09:49:29 +0530

    Varanasi data added

--- src/components/Cities/Varanasi/FaqsVaranasi.jsx
+++ src/components/Cities/Varanasi/FaqsVaranasi.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "../../Styles/Faq.css";
+import { varanasifaqs } from "./Images";
 
 const FaqsVaranasi = () => {
   // State to track which FAQ item is expanded
@@ -16,31 +17,12 @@ const FaqsVaranasi = () => {
     }
   };
 
-  // Sample FAQ data
-  const faqData = [
-    {
-      question: "What destinations do you offer?",
-      answer:
-        "We offer a wide range of destinations across the globe including popular tourist spots, off-the-beaten-path locations, and adventure destinations.",
-    },
-    {
-      question: "How can I book a trip?",
-      answer:
-        "You can book a trip through our website by selecting your desired destination, dates, and preferences, and completing the booking process online.",
-    },
-    {
-      question: "Are there any discounts available?",
-      answer:
-        "Yes, we offer discounts and special offers on select trips. Keep an eye on our website and subscribe to our newsletter to stay updated.",
-    },
-    // Add more FAQ items as needed
-  ];
 
   return (
     <div className="faq-container" id="faqs">
       <h1>Frequently Asked Questions</h1>
       <div className="faq-list">
-        {faqData.map((item, index) => (
+        {varanasifaqs.map((item, index) => (
           <div key={index} className="faq-item">
             <div
               className={`faq-question ${expandedItem === index && "expanded"}`}
